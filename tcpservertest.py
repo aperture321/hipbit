4ff2d49905c0d3a709cddd7f326d994419dca10a
Thomas Turney <aperture321@gmail.com>
2013-11-17 19:49:29 -0500

    Created tcp testing platform.

---
+++ tcpservertest.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+import os
+
+TCP_IP = ''
+TCP_PORT = 1221
+BUFFER_SIZE = 1024
+currentamt = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+
+while True:
+  conn, addr = s.accept()
+  print "Connection address: ", addr
+  f = open('rec.db', 'wb')
+  while 1:
+    limit = conn.recv(10) #this will get your limit size
+    other = limit[limit.find("**")+2:] #save extra packet stuff
+    limit = limit[:limit.find("**")] #get byte amount
+    limit = int(limit)
+    f.write(other)
+    print limit
+    l = conn.recv(BUFFER_SIZE) #get byte bufferred back
+    while (1):
+      if currentamt >= limit:
+        f.write(l)
+        break
+      f.write(l)
+      l = conn.recv(BUFFER_SIZE)
+      currentamt += BUFFER_SIZE
+    currentamt = 1024 #reassign values
+    limit = ''
+    break
+  f.close() #close your file after you're done
+  conn.close()
+
+s.close()
---
+++ tcptest.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#tcp tester
+
+import socket
+import sys
+import os
+import time
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 1221
+BUFFER_SIZE = 1024
+try:
+  limit = os.path.getsize("test.db") #4096 bytes
+  currentamt = 0
+
+  f = open("test.db", "rb")
+  l = f.read(BUFFER_SIZE)
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((TCP_IP, TCP_PORT))
+  s.send(str(limit) + "**") #first, send the limit with termination character
+  while(1):
+    s.send(l)
+    if currentamt >= limit:
+      break
+    l = f.read(BUFFER_SIZE)
+    currentamt += BUFFER_SIZE
+    print "still sending.."
+
+  s.close()
+
+  print "Done."
+  time.sleep(3)
+except:
+  print "Unexpected error:", sys.exc_info()[0]
+  time.sleep(3)
