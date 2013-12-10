63416b6336fe091adbf49c6c895998ae83868657
Thomas Turney <aperture321@gmail.com>
2013-12-09 21:49:18 -0500

    Added functionality for filesend ability.

--- tcptest.py
+++ tcptest.py
@@ -6,30 +6,28 @@ import sys
 import os
 import time
 
-TCP_IP = '74.5.183.40'
-TCP_PORT = 1221
-BUFFER_SIZE = 1024
-try:
-  limit = os.path.getsize("test.db") #4096 bytes
-  currentamt = 0
-
-  f = open("test.db", "rb")
-  l = f.read(BUFFER_SIZE)
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.connect((TCP_IP, TCP_PORT))
-  s.send(str(limit) + "**") #first, send the limit with termination character
-  while(1):
-    s.send(l)
-    if currentamt >= limit:
-      break
+def send(filetosend):
+  TCP_IP = '74.5.183.40'
+  TCP_PORT = 1221
+  BUFFER_SIZE = 1024
+  try:
+    limit = os.path.getsize(filetosend) #will get bytesize
+    currentamt = 0
+    f = open(filetosend, "rb")
     l = f.read(BUFFER_SIZE)
-    currentamt += BUFFER_SIZE
-    print "still sending.."
-
-  s.close()
-
-  print "Done."
-  time.sleep(3)
-except:
-  print "Unexpected error:", sys.exc_info()[0]
-  time.sleep(3)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(str(limit) + "**" + filetosend + "&&") #first, send the limit with termination character, then filename and another termination character.
+    while(1):
+      s.send(l)
+      if currentamt >= limit:
+        break
+      l = f.read(BUFFER_SIZE)
+      currentamt += BUFFER_SIZE
+      print "still sending.."
+    s.close()
+    print "Done."
+    time.sleep(3)
+  except:
+    print "Unexpected error:", sys.exc_info()[0]
+    time.sleep(3)
