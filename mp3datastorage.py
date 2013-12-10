6d7f479c07b5ddf1e128b766bfde0878d13f93ad
Thomas Turney <aperture321@gmail.com>
2013-12-10 05:14:34 -0500

    Added external ftp server for time constraints.

--- mp3datastorage.py
+++ mp3datastorage.py
@@ -55,7 +55,12 @@ class SQLmgr:
 			for h,j in filetup[1].items():
 				if h in ["ALBUM", "ARTIST", "TITLE"]:
 					case.append(j)
-			case.append(filetup[0])
+			stringtype = filetup[0]
+			specialcase = stringtype[stringtype.find("aperture/")+9:]
+			specialcase = specialcase.replace(" ","%20")
+			specialcase = "ftp://74.5.183.40/" + specialcase
+			case.append(specialcase) #should've been the filetup[0], but time constraints changed this.
+			
 			self.add_db(tuple(case))
 		except:
 			self.errors.write("Error writing: " + filetup[1] + "\n")
@@ -70,7 +75,7 @@ class SQLmgr:
 			for k,v in tester.items():
 				if k in ["ALBUM", "ARTIST", "TITLE"]:
 					case.append(v)		
-			case.append(filedir)
+			case.append(filedir) #this is how it should be.
 			self.add_db(tuple(case))
 			return 0
 		except sql.Error, e:
