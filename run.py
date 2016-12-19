ae6c7044ff2e9a2f60a99c99acf2ecd317c9f440
Thomas Turney <aperture321@gmail.com>
2016-12-18 21:19:14 -0800

    added runner, initial arg parsing.

---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Thomas Turney 2017
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Run or build application.')
+parser.add_argument("test", metavar='Test')
+parser.parse_args()
